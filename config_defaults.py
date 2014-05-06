c8670755e9a6e5accf1298d799a1eaaab40787aa
Andrew Plummer <plummer574@gmail.com>
2014-05-06 21:25:00 +0100

    Initial commit

---
+++ config_defaults.py
@@ -0,0 +1,5 @@
+DEBUG = False
+SECRET_KEY = 'secret_key'
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'password'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://recipe:password@localhost/recipe'
---
+++ mysql_init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS recipe;
+GRANT ALL ON recipe.* TO 'recipe'@'localhost' IDENTIFIED BY 'password';
